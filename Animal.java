72ec2b796e358b6b53df21573ca68be755ffbbf3
Liam Stabeno and Nathaniel Meyer <liam@>
2017-03-13 10:44:20 -0700

    Add Animal class and App.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+class public Animal {
+  public int mLegCount;
+  public String mSpecies;
+  public String mBreed;
+  public String mName;
+  public int mAge;
+  public int mWeight;
+  public int mHeight;
+  public int mLength;
+}
