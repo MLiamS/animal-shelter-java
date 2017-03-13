00c0e200d71305bf7beba7901b8956003dcac171
Liam Stabeno and Nathaniel Meyer <nathaniel@>
2017-03-13 13:56:01 -0700

    Add Animal constructor and refactor App.java

--- Animal.java
+++ Animal.java
@@ -8,6 +8,17 @@ class Animal {
   public int mHeight;
   public int mLength;
 
+  public Animal(int legCount, String species, String breed, String name, int age, int weight, int height, int length) {
+    mLegCount = legCount;
+    mSpecies = species;
+    mBreed = breed;
+    mName = name;
+    mAge = age;
+    mWeight = weight;
+    mHeight = height;
+    mLength = length;
+  }
+
   public boolean insidePet(int maxHeight) {
     return (mHeight < maxHeight);
   }
--- App.java
+++ App.java
@@ -4,35 +4,10 @@ public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
 
-    Animal dog = new Animal();
-    dog.mLegCount = 3;
-    dog.mSpecies = "Canis lupus familiaris";
-    dog.mBreed = "Labrador retriever";
-    dog.mName = "Tripod";
-    dog.mAge = 2;
-    dog.mWeight = 35;
-    dog.mHeight = 17;
-    dog.mLength = 50;
+    Animal dog = new Animal(3, "Canis lupus familiaris", "Labrador retriever",  "Tripod", 2, 35, 17, 50);
+    Animal cat = new Animal(4, "Felis catus", "Domestic shorthair",  "Moe", 4, 15, 1, 2);
+    Animal snake = new Animal(0, "Pantherophis guttatus", "Anery amely",  "Concetta",  6, 2, 1, 5);
 
-    Animal cat = new Animal();
-    cat.mLegCount = 4;
-    cat.mSpecies = "Felis catus";
-    cat.mBreed = "Domestic shorthair";
-    cat.mName = "Moe";
-    cat.mAge = 4;
-    cat.mWeight = 15;
-    cat.mHeight = 1;
-    cat.mLength = 2;
-
-    Animal snake = new Animal();
-    snake.mLegCount = 0;
-    snake.mSpecies = "Pantherophis guttatus";
-    snake.mBreed = "Anery amely";
-    snake.mName = "Concetta";
-    snake.mAge = 6;
-    snake.mWeight = 2;
-    snake.mHeight = 1;
-    snake.mLength = 5;
 
     Animal[] allAnimals = {dog, cat, snake};
 
